f140c6b43cf3e8c176b6fe0c55b7e6491f305933
cfogrady <cfogrady@gmail.com>
2024-12-19 22:02:07 -0500

    Basic Character Parsing

--- vb-nfc-reader/src/main/java/com/github/cfogrady/vbnfc/NfcCharacter.kt
+++ vb-nfc-reader/src/main/java/com/github/cfogrady/vbnfc/NfcCharacter.kt
@@ -3,4 +3,22 @@ package com.github.cfogrady.vbnfc
 data class NfcCharacter(
     val dimId: UShort,
     val slotId: UShort,
-)
+    val abilityRarity: AbilityRarity,
+    val abilityId: UShort,
+    val mood: UByte,
+    val vitals: UShort,
+    val transformationTimer: UShort,
+    val hpTraining: UShort,
+    val apTraining: UShort,
+    val bpTraining: UShort,
+    val trainingLimit: UShort
+) {
+    enum class AbilityRarity {
+        None,
+        Common,
+        Rare,
+        SuperRare,
+        SuperSuperRare,
+        UltraRare,
+    }
+}
