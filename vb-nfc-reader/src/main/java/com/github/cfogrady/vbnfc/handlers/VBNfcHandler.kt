ad51df7cb184222138d03573059838a0240a3ca2
cfogrady <cfogrady@gmail.com>
2024-12-19 22:03:02 -0500

    Write character

--- vb-nfc-reader/src/main/java/com/github/cfogrady/vbnfc/handlers/VBNfcHandler.kt
+++ vb-nfc-reader/src/main/java/com/github/cfogrady/vbnfc/handlers/VBNfcHandler.kt
@@ -3,7 +3,9 @@ package com.github.cfogrady.vbnfc.handlers
 import android.nfc.NfcAdapter
 import android.nfc.tech.NfcA
 import android.util.Log
+import com.github.cfogrady.vbnfc.ConvertToPages
 import com.github.cfogrady.vbnfc.NfcCharacter
+import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 import java.nio.charset.StandardCharsets
 import java.util.Base64
@@ -31,6 +33,7 @@ abstract class VBNfcHandler(private val secrets: Secrets, private val nfcData: N
         const val OPERATION_READY: Byte = 1
         const val OPERATION_TRANSFERRED_TO_APP: Byte = 2
         const val OPERATION_CHECK_DIM: Byte = 3
+        const val OPERATION_TRANSFERED_TO_DEVICE: Byte = 4
         const val START_DATA_PAGE = 8
         const val LAST_DATA_PAGE = 220 // technically 223, but we read 4 pages at a time.
 
@@ -65,12 +68,47 @@ abstract class VBNfcHandler(private val secrets: Secrets, private val nfcData: N
     }
 
     fun prepareDIMForCharacter(dimId: UShort) {
-
+        // set app nonce to device ensure when we send back the character that we are preparing
+        // the same device we send to
+        nfcData.transceive(getOperationCommandBytes(OPERATION_READY))
+        // app authenticates, and reads everything, and checks the version
+        // The version check is only for the BE when transfering from DIM=0 (pulsemon).
+        // This was from the bug when the BE first came out.
+        // Check (page 103 [0:1] != 1, 0)
+        nfcData.transceive(getOperationCommandBytes(OPERATION_CHECK_DIM))
     }
 
+    @OptIn(ExperimentalStdlibApi::class)
     fun sendCharacter(character: NfcCharacter) {
+        // check the nonce
+        // if it's not expected, then the bracelet isn't ready
+
+        // Check the product and device ids that they match the target.
+        // This check relies on the app categories of BE, Vital Hero, and Vital Series.
+
+        // ensure the dim id matches the expected
+        if (character.dimId != getHeaderDimId()) {
+            throw IllegalArgumentException("Device is ready for DIM ${getHeaderDimId()}, but attempted to send ${character.dimId}")
+        }
+
+        // update the memory data
+        nfcData.transceive(getOperationCommandBytes(OPERATION_READY))
+        passwordAuth()
+
+        // write nfc data
+        val myCharacter = "551abd291a96404cb38cd99babe1184b43eb2479a65c59ebd7e0f3246845a54c5f9dc34770bc69e18ba7bc147c18516f3524c78631f16496e4492e97f74a289105d83d6cf1908eb66565fd4018986ef20862ce4a9376efcb001da75135c5c3fd93f4b9dad14d46c92f192c8e39cbd669c72f27d273a6ddcf554203761924dc1697f5e592c01dcdcc335a4f3eb89cd478907449b3401c0ae9cd0c56cc140a35dcc3dc9ad17a475ac884129d662e4525c14349d1ce507d59a34b1dbe60d1f7d8842da3f1f4604710eef661c3de5291cf33e5569e5abacf9b601d45eee68a7b6eaff0f21724e90cad6b721244d57d78da67d735a0e73deb431d6fb1b8fb755787ee51e8a20da957f97d62c408e6572f865be94aca43fa19116ac318fd2700f093011438f47eaac2e85d538042ff93ce6bc333589f6d341555d36fbba74567ef613d2242c9471f2ed7aa3e565cecd46fa2ca8f88a022c714fc04f0f69c43e74af7ad7bdec18794d9bc03e2e06773a54c3ddbfe76ee5f96845293ef8fc68653831c405edbd8594a620502cb506770a89ba999d4df0d23c40d021e7c2e7159d2f380cbeb77c5d97c9b503503e7dbbae37c42a4a2a4985cabbbc979d42aaac4651c95c41dc4b0baec7d7199e9d929836e13d3e3f8c958facb305f339992162ab0258b42dfa4a02ad3ec28d48c60909e008c5fb2b2e976942784e098313d75e8a6eec78d559aa2617c35574c7a0c5f8f7e103c77087b213d6b02c444a1fb4eab23a967ddac02f9b959675c69f3c97894929c4d5a2779444b05246da35744aab4b545eefc9478620d6e1fb584a9f79080236e005cf738228c104570c200e11b6307a2d236c6031b0f89da38ce1b401eed4dda2b78268d316115b2e9c222441d9cd8d4b559adb9f1ce4a8f2a0ed3f55034e47c778d85654ac1e5048326827f0db6103cfcce491d6bf084a9e7ee2a1446c616c87f1246fb2da22a6eaae4167814f24230aed64e342cae18742c21e4b2f2c600ce783f7ec33f6937a2224a366cd1feb53011e766d0d5459d1b83a1a9147be8ddc8fea75bbdeac18077aaa469898354ff400608e2dac32be82ff7528252c0da681d2fe78150f6020c3691b5cc6cb81285f6716acf0040b346d1b76cd61bc08377f67a8adc341ceda34585ddefeee1dfc416d05067015984e3619bcbc188d8d49ab4024c9e1b83a6c0fd91b4e835d59b3f59d97d".hexToByteArray()
+        val pagedData = ConvertToPages(myCharacter)
+        for(pageToWriteIdx in 8..<pagedData.size) {
+            val pageToWrite = pagedData[pageToWriteIdx]
+            nfcData.transceive(byteArrayOf(NFC_WRITE_COMMAND, pageToWriteIdx.toByte(), pageToWrite[0], pageToWrite[1], pageToWrite[2], pageToWrite[3]))
+        }
+
+
+        nfcData.transceive(getOperationCommandBytes(OPERATION_TRANSFERED_TO_DEVICE))
 
     }
+
+    abstract fun getHeaderDimId(): UShort
 
     abstract fun getOperationCommandBytes(operation: Byte): ByteArray
 
@@ -79,8 +117,6 @@ abstract class VBNfcHandler(private val secrets: Secrets, private val nfcData: N
         val encoded = Base64.getUrlEncoder().encodeToString(value.toByteArray())
         return encoded
     }
-
-    data class VBNfcHeader(val magic: UInt, val itemId: UShort, val itemNumber: UShort)
 
     data class Secrets(val passwordKey1: String, val passwordKey2: String, val decryptionKey: String, val substitutionCypher: IntArray)
 
